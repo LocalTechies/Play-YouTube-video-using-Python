d9d9e5d1fdf8920bfe50ee08c29d5124fc259112
LocalTechies <65487808+LocalTechies@users.noreply.github.com>
2020-06-13 15:56:28 +0530

    Create PlayYouTube.py

---
+++ PlayYouTube.py
@@ -0,0 +1,9 @@
+#install pywhatkit
+#Anaconda users --> conda install pywhatkit
+#cmd or PyCharm ---> pip install pywhatkit
+
+import pywhatkit as kit
+
+#Calling the playonyt method from pywhatkit
+
+kit.playonyt("Local Techies") #The default browser will be opened and the latest/updated video on the top will be played.
